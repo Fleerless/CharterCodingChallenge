55b7b709d9842ed8bb081a9652e7ad7e52bc999d
Jeffrey Fleer <jeffreyfleer+github@gmail.com>
2020-10-21 09:45:19 -0600

    api call returning data and stored to state

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -7,6 +7,27 @@ class Table extends Component {
 
         }
     }
+
+    //  Other Functions
+    componentDidMount() {
+        fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
+            headers: {
+                Authorization: "Api-Key q3MNxtfep8Gt",
+            },
+        })
+            // Parse the response
+            .then(function (response) {
+                return response.json();
+            })
+            // Save to State
+            .then((data) => {
+                // sort data by name property and save to state
+                this.setState({
+                    originalRestaurants: (data.sort((a, b) => (a.name > b.name) ? 1 : -1)),
+                    availableRestaurants: (data.sort((a, b) => (a.name > b.name) ? 1 : -1))
+                })
+            })
+    }
     render() {
         return(
             <div></div>
